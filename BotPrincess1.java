3434f1efbd2e8f1e935ba0158241f1d5cb0a310b
Ankush Sharma <ankushors789@gmail.com>
2017-11-29 01:11:30 +0530

    First code Commit
    Initial code in java

---
+++ BotPrincess1.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+
+        char grid[][] = new char[m][m];
+        for(int i = 0; i < m; i++)
+        {
+            String line = br.readLine();
+            grid[i] = (line.trim()).toCharArray();
+        }
+
+        displayPathtoPrincess(m, grid);
+    }
+
+    static void displayPathtoPrincess(int m, char[][] grid)
+    {
+        int botRow = 0, botCol = 0, princessRow = 0, princessCol = 0;
+
+        // Get bot and princess coordinates
+        for (int r = 0; r < m; r++)
+        {
+            for (int c = 0; c < grid.length; c++)
+            {
+                if (grid[r][c] == 'm' || grid[r][c] == 'M')
+                {
+                    botRow = r;
+                    botCol = c;
+                }
+                else if (grid[r][c] == 'p' || grid[r][c] == 'P')
+                {
+                    princessRow = r;
+                    princessCol = c;
+                }
+            }
+        }
+
+        // Move the bot up or down till bot reaches same row
+        if( princessRow < botRow )
+        {
+            while(botRow != princessRow)
+            {
+                botRow--;
+                System.out.println("UP");
+            }
+        }
+        else if( princessRow > botRow )
+        {
+            while(botRow != princessRow)
+            {
+                botRow++;
+                System.out.println("DOWN");
+            }
+        }
+
+        // Move the bot left or right till bot reaches same column
+        if( princessCol < botCol )
+        {
+            while(botCol != princessCol)
+            {
+                botCol--;
+                System.out.println("LEFT");
+            }
+        }
+        else if( princessCol > botCol )
+        {
+            while(botCol != princessCol)
+            {
+                botCol++;
+                System.out.println("RIGHT");
+            }
+        }
+
+    }
+
+}
